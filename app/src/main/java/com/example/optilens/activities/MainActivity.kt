5562e626f3179bc5d7c97b125f33bc732a912f61
mitiljawale <mvjawale17@gmail.com>
2023-11-09 02:17:47 -0500

    Mitil Commit - Splash Screen Added

--- app/src/main/java/com/example/optilens/activities/MainActivity.kt
+++ app/src/main/java/com/example/optilens/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.optilens
+package com.example.optilens.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.optilens.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
