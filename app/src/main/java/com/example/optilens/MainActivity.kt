3ea8892f95e940b88a41b767d5476192d3190654
mihikagavali <mihikagavali@gmail.com>
2023-11-09 14:26:34 -0500

    Mihika trial - added a comment

--- app/src/main/java/com/example/optilens/MainActivity.kt
+++ app/src/main/java/com/example/optilens/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //bullcrap
+        //Mihika - Naithik is chutiya
         // Mitil branch created
     }
 }
