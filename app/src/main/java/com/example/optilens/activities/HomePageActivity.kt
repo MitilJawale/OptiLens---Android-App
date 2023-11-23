b9a6528bd62ffe29e593ae7a034de7387b360803
mitiljawale <mvjawale17@gmail.com>
2023-11-22 19:27:11 -0500

    HomeActivity Added HomeFragment Added
    - Changed LAUNCHER Activity
    - Main Activity -> SplashScreenActivity -> HomePageActivity -> HomePageFragment

--- app/src/main/java/com/example/optilens/activities/HomePageActivity.kt
+++ app/src/main/java/com/example/optilens/activities/HomePageActivity.kt
@@ -8,18 +8,27 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.optilens.R
+import com.example.optilens.fragments.HomeFragment
 
 class HomePageActivity : AppCompatActivity() {
+
+    private lateinit var btnLogout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
         // Find the Toolbar in your layout
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        btnLogout = findViewById(R.id.btn_logout)
 
         // Set the Toolbar as the app bar for the activity
         setSupportActionBar(toolbar)
@@ -27,8 +36,19 @@ class HomePageActivity : AppCompatActivity() {
         // Avoids the Regular Project Name from displaying on the top
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        loadFragment(HomeFragment())
+
+    }
 
 
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        // Replace the existing fragment with the new one
+        fragmentTransaction.replace(R.id.frame_layout_main, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 
 
