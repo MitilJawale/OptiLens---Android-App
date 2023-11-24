6ced93eaa854eafbc25e3dbe413c5e47db4966c1
dannypandit <dannypandit1234@gmail.com>
2023-11-24 03:15:53 -0500

    Revisited Register and Completed Profile.

--- app/src/main/java/com/example/optilens/dataclass/User.kt
+++ app/src/main/java/com/example/optilens/dataclass/User.kt
@@ -3,23 +3,18 @@ package com.example.optilens.dataclass
 import java.util.*
 
 data class User(
-    val userId: String?=null,
     val name: String? = null,
     val address: String? = null,
     val phoneNumber: String? = null,
     val email: String? = null,
     val password: String? = null,
     val gender: String? = null,
-    val profilePicture: String? = null,
+    val profilePicture: String? = "",
     val wishlist: List<WishlistItem> = emptyList(),
     val cart: List<CartItem> = emptyList(),
     val orders: List<Order> = emptyList(),
     val lensPrescription: LensPrescription? = null
-){
-    fun updateGender(newGender: String?): User {
-        return this.copy(gender = newGender)
-    }
-}
+)
 
 data class WishlistItem(
     val productId: Int,
@@ -49,10 +44,10 @@ data class OrderedProduct(
 )
 
 data class LensPrescription(
-    val sphRight: Double? = null,
-    val cylRight: Double? = null,
-    val axisRight: Int? = null,
-    val sphLeft: Double? = null,
-    val cylLeft: Double? = null,
-    val axisLeft: Int? = null
+    val sphRight: String? = null,
+    val cylRight: String? = null,
+    val axisRight: String? = null,
+    val sphLeft: String? = null,
+    val cylLeft: String? = null,
+    val axisLeft: String?=null
 )
