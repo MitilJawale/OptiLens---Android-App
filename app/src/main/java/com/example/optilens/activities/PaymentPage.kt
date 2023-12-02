44b073cb578e2dda373078f7e814b93a5944b597
mihikagavali <mihikagavali@gmail.com>
2023-12-01 23:51:21 -0500

    Payment Activity Added and Working Sample Cart and Checkout Activity added(will update later)

--- app/src/main/java/com/example/optilens/activities/PaymentPage.kt
+++ app/src/main/java/com/example/optilens/activities/PaymentPage.kt
@@ -1,20 +1,21 @@
 package com.example.optilens.activities
 
-
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.braintreepayments.cardform.view.CardForm
 import com.example.optilens.R
 
-
-
 class PaymentPage : AppCompatActivity() {
 
-    private lateinit var cardForm : CardForm
-    private lateinit var btnBuy : Button
+    private lateinit var cardForm: CardForm
+    private lateinit var btnBuy: Button
+    private lateinit var btnBack: ImageButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,7 @@ class PaymentPage : AppCompatActivity() {
 
         cardForm = findViewById(R.id.cardForm)
         btnBuy = findViewById<Button>(R.id.btnBuy)
+        btnBack = findViewById<ImageButton>(R.id.btn_back)
         cardForm.cardRequired(true)
             .expirationRequired(true)
             .cvvRequired(true)
@@ -33,19 +35,26 @@ class PaymentPage : AppCompatActivity() {
             .mobileNumberExplanation("Mobile Number is required for SMS verification")
             .setup(this@PaymentPage)
 
-        cardForm.cvvEditText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        cardForm.cvvEditText.inputType =
+            InputType.TYPE_CLASS_NUMBER or InputType.TYPE_TEXT_VARIATION_PASSWORD
 
-        btnBuy.setOnClickListener(){
-            if(cardForm.isValid){
-                Toast.makeText(this@PaymentPage, "Payment Done"
-                    ,Toast.LENGTH_LONG).show()
-            }
-            else{
-                Toast.makeText(this@PaymentPage,"Please fill all details",Toast.LENGTH_LONG).show()
+        btnBuy.setOnClickListener() {
+            if (cardForm.isValid) {
+                Toast.makeText(
+                    this@PaymentPage, "Payment Done", Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(this@PaymentPage, "Please fill all details", Toast.LENGTH_LONG)
+                    .show()
             }
         }
 
+        btnBack.setOnClickListener() {
+            val intent = Intent(this , CheckoutActivity::class.java)
+            startActivity(intent)
 
-
+            finish()
+        }
     }
+
 }
